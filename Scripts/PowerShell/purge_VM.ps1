bc9da8c9821f2d783fddc15898b8242f26857e59
frgnca <fg@frgn.ca>
2017-06-06 12:43:16 -0400

    Create purge_VM.ps1

---
+++ Scripts/PowerShell/purge_VM.ps1
@@ -0,0 +1,64 @@
+<#
+  Copyright (c) 2017 Francois Gendron <fg@frgn.ca>
+  GNU Affero General Public License v3.0
+
+  purge_VM.ps1
+  PowerShell script that purges a virtual machine
+#>
+
+################################################################################
+# Set new virtual machine parameters
+$VirtualMachineName = "test1"
+$VirtualMachineLocation = "D:\VMs"
+########################
+# Display VM parameters
+Write-Host "
+
+########################
+
+         Name: $VirtualMachineName 
+Only continue if the parameters are correct"
+
+# Chance to stop before proceeding
+Pause
+
+# If virtual machine exists
+$exist = $null
+$exist = Get-VM | Where-Object -Property Name -EQ $VirtualMachineName
+if($exist -ne $null)
+{
+    # Display instructions
+    Write-Host "
+########################
+
+Wait time  1+min (100% when done)"
+
+    # Stop virtual machine
+    Stop-VM $VirtualMachineName -Force -TurnOff
+
+    # While virtual machine is not off
+    while((Get-VM $VirtualMachineName | Select-Object -Property State).State -ne "Off")
+    {
+        # Wait for a second
+        sleep(1)
+    }
+
+    # Delete virtual machine from Hyper-V
+    Remove-VM $VirtualMachineName -Force
+}
+
+# Delete virtual machine folder
+$exist = $null
+$exist = Test-Path "$VirtualMachineLocation\$VirtualMachineName"
+if($exist)
+{
+    Remove-Item -Recurse -Force "$VirtualMachineLocation\$VirtualMachineName"
+}
+
+# Display instructions
+Write-Host "
+########################
+
+Done
+
+########################"
